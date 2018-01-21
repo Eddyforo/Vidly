aece37835f33b9f11676714972035a71550efdb4
Eddy Fonseca <eddy.fonseca@softland.cr>
2018-01-21 01:54:28 -0600

    Antes del quiz decla segunda seccion

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("customers")]
+        public ActionResult Customers()
+        {
+            var customers = new Customers() { Name = "Customer 1!" };
+
+             
+            return View(customers);
+        }
+    }
+}
