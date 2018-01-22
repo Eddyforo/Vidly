a2b0ef4088d3019c50edc738c9d5789a0203a278
Eddy Fonseca <eddy.fonseca@softland.cr>
2018-01-21 22:18:05 -0600

    PopulateMemberShipType

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -10,15 +7,30 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-
-            var customers = new List<Customers>
+            var customers = new List<Customer>
             {
-                new Customers {Name = "Customer 1"},
-                new Customers {Name = "Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -28,33 +40,6 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}" )]
-         public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + 1);
-        }
-
-        //movies
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
--- Vidly/Models/Customers.cs
+++ Vidly/Models/Customers.cs
@@ -5,9 +5,13 @@ using System.Web;
 
 namespace Vidly.Models
 {
-    public class Customers
+    public class Customer
     {
-        public int id { get; set; }
-        public string Name { get; set; } 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MemberShipType MembershipType { get; set; }
+
+        public byte MembershipTypeId { get; set; }
     }
 }
